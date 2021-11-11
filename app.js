b4b07baacfad20cd35133818b7ea40af2231ed69
jeondaegam <jeondaegam@naver.com>
2021-11-11 15:45:14 +0900

    Variable

---
+++ app.js
@@ -0,0 +1,31 @@
+alert("Hey, you are doing well" +
+    " \n You can do it");
+
+console.log(99);
+console.log('okay');
+console.log("hey");
+
+//let something;
+//console.log(something); // undefined
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+console.log(daysOfWeek);
+
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+
+/* 값을 리스트로 정리 */
+const toBuy = ["potato", "tomato", "pizza"];
+console.log(toBuy[2]);
+console.log(toBuy[2235235]);
+
+
+/*
+console
+- js를 쉽게 다룰 수 있다.
+- 뭘 하거나 설치할 필요없이 브라우저에서 바로 사용하면 되는 것이다.
+- js와 상호작용하긴 좋은데 긴 코드나 프로그램을 작성해야할 땐 적절치 않다.
+ */
+
+
