d1a705af656ac19a3760c2c6baebc535b804f425
jeondaegam <jeondaegam@naver.com>
2021-11-15 15:48:11 +0900

    Recap & Calculator

--- README.md
+++ README.md
@@ -5,34 +5,54 @@ my second Js project with Nico
 2.2 variable 의 가치  
 const : 바뀌지 않는 값. 상수(constant)
 
-2.3 const & let  
+2.3 데이터를 만들 수 있는 두가지 방법  
+const & let  
 const > 상수이므로 값을 변경할 수 없다.
 let > 업데이트될 수 있는 변수.
+```javascript
+const isChudongFat = true;
+isChudongFat = false; // (X) const로 만들었기 때문에 업데이트 불가능
+
+// 대신 let을 사용하자.
+let isJeonFat = true; 
+isJeonFat= false; // (O) const와 달리 값을 업데이트 할 수 있다.
+```
+
 이런 코드 규칙의 중요성 > 코드를 통해 의도를 알 수 있다.
 
-규칙 > 기본적으로 const를 쓰고 필요할 때만 let을 쓰되, var은 쓰지 말 것.
-always const
-sometimes let
-never var
+규칙 > 항상 const를 사용하되, 필요하다면 let을 쓰고, var은 쓰지 말 것.  
+Always const, Sometimes let, Never var!
 
 과거에는 var을 사용했는데
 var를 사용한다면 언어를 통한 보호를 받지 못해 실수로 값을 변경해도 언어가 말해주지 않기때문이다.
 var는 어떠한 규칙도 가지고 있지 않다! 따라서 오래된 코드에는 var가 사용된 것을 볼 수 있다.
 모두 var을 사용한다면 코드가 뭘 하려는지 알 수 없다.
+let, const처럼 구분을 할 수 없게 된다.
+우리가 코드를 읽으면서 어떤 의미를 얻을 수 있다면 그게 더 바람직할 것이다.
 
 
 2.4 Boolean
 null : 값이 없음 (아무것도 없는 상태로 채워짐)
-- 절대 자연적이지 않을 값. 여기에 값이 없다는 것을 알려줄 때 사용한다.!
-- 일부러 비워둠. 빈 값으로 정의함
-undefined : 변수는 만들었지만 값이 정의되지 않음.
-메모리 안에는 존재해, 공간은 있는데 아무런 값이 없음음.
+- 빈 값으로 정의함.
+- 변수에 할당되는 값. variable 안에 아무 것도 없다는 것을 확실히 할 때 사용한다.  
+undefined : 변수는 만들었지만 값을 부여하지 않은 상태.
+메모리 안에는 존재해, 공간은 있는데 아무런 값이 없음.
 
 
 2.5 Arrays
 - 데이터를 정리할 수 있는 방법
 - 가장 기본적인 데이터 구조
 - 리스트화 하고 그룹화 하기 위함.
+```javascript
+const mySoulFood = ["pizza", "potato", "tomato"];
+mySoulFood[2] = "what";
+console.log(mySoulFood);
+```
+####Push
+가장 끝에 item을 하나 추가한다.
+```javascript
+mySoulFood.push("burger");
+```
 
 ###2.6 Objects
 여러개의 값에 이름을 붙여 사용하고자 할 때 사용한다.
@@ -65,5 +85,21 @@ console.log(player.name);
 console.log(player["lastName"]);
 ```
 
+###2.7 Functions 
+계속 반복해서 사용할 수 있는 코드 조각  
+어떤 코드를 캡슐화해서, 계속 반복해서 사용할 수 있도록 해준다.
+노래나 동영상을 계속 반복재생 하는 것처럼 계속해서 같은 행동을 실행한다.  
+
+argument=> function을 실행하는 동안 어떤 정보를 function에게 보낼 수 있는 방법.
 
 2시반까지 ~2.10
+
+error message
+NaN   
+Not a Number (숫자가 아님)
+
+###2.9 Recap
+Js는 위에서 아래로 읽는다.
+
+
+
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-alert("Hey, you are doing well" +
-    " \n You can do it");
+/*alert("Hey, you are doing well" +
+    " \n You can do it");*/
 
 console.log(99);
 console.log('okay');
@@ -48,3 +48,87 @@ player.lastName = "jeon";
 // object를 하나의 값으로서 업데이트 하는 것은 불가능
 //player = false; // (X)
 console.log(player);
+
+// 2.7 functions
+sayHello("Cudong", 10);
+sayHello("Yeoreum", 23);
+sayHello("Sangyoon", 21);
+sayHello("Changsoon", 25);
+
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+
+// 계산기 function 만들어보장
+function plus (firstNum,secondNum){
+    console.log(firstNum + secondNum);
+}
+function divide(a, b) {
+    console.log(a/b);
+}
+plus(8,60);
+divide(98,20);
+
+// object 안에 function 만들기
+const user = {
+    name:"diniyo",
+    sayHello: function(otherPersonName){
+        console.log("Hello! " + otherPersonName + " nice to meet you");
+    },
+};
+
+console.log(user.name);
+user.sayHello("lynn"); // -> function이 user object 안에 있을 때만 사용 가능하다.
+user.sayHello("jeon");
+
+
+// 2.9 Recap
+// 데이터를 만들 수 있는 두가지 방법
+const a = 5;
+
+const isChudongFat = true;
+// isChudongFat = false; // const로 만들었기 때문에 업데이트 불가능
+
+// 대신 let을 사용하자.
+let isJeonFat = true; // const와 달리 값을 업데이트 할 수 있다.
+isJeonFat= false;
+
+// console엔 수많은 property가 있고 그중 하나가 log이다. 즉 console은 하나의 object이다.
+// player라는 variable 안에 name이라는 variable(=property)을 만든 것.
+/*
+const player = {
+    name: "Okay",
+}*/
+
+console.log(console);
+
+function minusFive(potato) {
+ console.log(potato-5);
+}
+minusFive(5,10,2,34,5,9,6);
+// 아주 많은 argument를 보내더라도 문제되지 않는다.
+// minusFive는 오직 첫번째 argument만 받는다.
+
+const calculator = {
+    add: function (a, b) {
+        console.log(a +"+"+b+"="+ Number(a+b));
+    },
+    minus: function (a,b) {
+        console.log(a+"-"+b+"="+ Number(a-b));
+    },
+    divide: function (a,b) {
+        console.log(a+"/"+b+"="+a/b);
+    },
+    power: function (a,b) {
+        console.log(a+"^"+b+"="+a**b);
+    },
+    multiple: function (a,b) {
+        console.log(a+"*"+b+"="+a*b);
+    }
+}
+
+calculator.add(3,4);
+calculator.minus(5,1);
+calculator.divide(10,3);
+calculator.multiple(4,2);
+calculator.power(2,3);
